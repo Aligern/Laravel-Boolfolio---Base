699fb71bd888e1996a3ebbe3d278330d95fe8487
Aligern <sasso.luca93@gmail.com>
2024-05-30 18:17:45 +0200

    add slug function to project.php

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Support\Str;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,4 +10,8 @@ class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'content', 'slug'];
+
+    public static function generateSlug($title) {
+        return Str::slug($title, '-');
+    }
 }
