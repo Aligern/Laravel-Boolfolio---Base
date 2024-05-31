3812a40f5114ffa3a6650de9229dbc3c72c0e007
Aligern <sasso.luca93@gmail.com>
2024-05-31 19:37:40 +0200

    working on nav bar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,18 +4,10 @@
     </a>
     <ul class="nav flex-column">
         <li class="nav-item">
-          <a class="nav-link"  {{Route::currentRouteName() === 'admin.dashboard.index' ? 'active' : ''}} href="{{route('admin.projects.index')}}">
-            <i class="fa-solid fa-link pe-2"></i>Link
-        </a>
+          <a class="nav-link"  {{Route::currentRouteName() === 'admin.dashboard' ? 'active-' : ''}} href="{{route('admin.dashboard')}}"><i class="fa-solid fa-link pe-2"></i>Link</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link"  {{Route::currentRouteName() === 'admin.projects.index' ? 'active' : ''}} href="#"><i class="fa-solid fa-diagram-project pe-2"></i>Projects</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link"  {{Route::currentRouteName() === 'admin.projects.index' ? 'active' : ''}} href="{{route('admin.projects.index')}}"><i class="fa-solid fa-diagram-project pe-2"></i>Projects</a>
         </li>
       </ul>
 </nav>
